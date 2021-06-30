101e6f03768b8d2aee5c7c3ea32888f928340461
Guilherme Reis <grrbm2@gmail.com>
2021-06-30 13:23:40 -0300

    first commit

---
+++ scraper.py
@@ -0,0 +1,10 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+
+page = requests.get('https://web.archive.org/web/20121007172955/http://www.nga.gov/collection/anZ1.htm')
+
+soup = BeautifulSoup(page.text, 'html.parser')
+
+print(soup.get_text())
