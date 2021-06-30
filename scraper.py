04795447e49ae11317cd237184780a07ca27753f
Guilherme Reis <grrbm2@gmail.com>
2021-06-30 15:22:08 -0300

    added gecko driver

--- scraper.py
+++ scraper.py
@@ -1,11 +1,27 @@
-import requests
-import csv
+# Imports, of course
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 
+# Initialize a Firefox webdriver
+driver = webdriver.Firefox()
 
-page = requests.get('https://web.archive.org/web/20121007172955/http://www.nga.gov/collection/anZ1.htm')
+# Grab the web page
+driver.get("http://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/pgmei.app/Identificacao")
 
-soup = BeautifulSoup(page.text, 'html.parser')
+# We use .find_element_by_id here because we know the id
+text_input = driver.find_element_by_id("cnpj")
 
-print('will print something')
-print(soup.get_text())
+# Then we'll fake typing into it
+text_input.send_keys("38294699000112")
+
+# Now we can grab the search button and click it
+search_button = driver.find_element_by_class_name("btn btn-success ladda-button")
+search_button.click()
+
+# Instead of using requests.get, we just look at .page_source of the driver
+driver.page_source
+
+print("sucess")
