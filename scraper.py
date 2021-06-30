ca8ba7c8e2423ad540266386c866b2036963022b
Guilherme Reis <grrbm2@gmail.com>
2021-06-30 16:07:56 -0300

    logging exception

--- scraper.py
+++ scraper.py
@@ -1,3 +1,6 @@
+# errors
+import traceback
+import logging
 # Imports, of course
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -5,23 +8,29 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 
-# Initialize a Firefox webdriver
-driver = webdriver.Firefox()
+try:
+        
+    # Initialize a Firefox webdriver
+    driver = webdriver.Firefox()
 
-# Grab the web page
-driver.get("http://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/pgmei.app/Identificacao")
+    # Grab the web page
+    driver.get("http://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/pgmei.app/Identificacao")
 
-# We use .find_element_by_id here because we know the id
-text_input = driver.find_element_by_id("cnpj")
+    # We use .find_element_by_id here because we know the id
+    text_input = driver.find_element_by_id("cnpj")
 
-# Then we'll fake typing into it
-text_input.send_keys("38294699000112")
+    # Then we'll fake typing into it
+    text_input.send_keys("38294699000112")
 
-# Now we can grab the search button and click it
-search_button = driver.find_element_by_class_name("btn btn-success ladda-button")
-search_button.click()
+    # Now we can grab the search button and click it
+    search_button = driver.find_element_by_class_name("btn btn-success ladda-button")
+    search_button.click()
 
-# Instead of using requests.get, we just look at .page_source of the driver
-driver.page_source
+    # Instead of using requests.get, we just look at .page_source of the driver
+    driver.page_source
 
-print("sucess")
+    print("sucess")
+    
+except Exception as e:
+    logging.error(traceback.format_exc())
+    # Logs the error appropriately. 
