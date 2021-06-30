2c4b29fddbbd42e8c38cb4953731e1eab310edc0
Guilherme Reis <grrbm2@gmail.com>
2021-06-30 17:18:16 -0300

    added more arguments

--- scraper.py
+++ scraper.py
@@ -13,7 +13,10 @@ try:
         
     options = FirefoxOptions()
     options.binary_location = r"/app/vendor/firefox/firefox"
+    options.add_argument("--no-sandbox")
     options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
     # Initialize a Firefox webdriver
     driver = webdriver.Firefox(options=options, executable_path='/app/vendor/geckodriver/geckodriver')
 
