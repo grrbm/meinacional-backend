955081b70d657c88c715d69b0f67cecedd799f10
Guilherme Reis <grrbm2@gmail.com>
2021-09-03 14:53:48 -0300

    saving pdf to path with autoit

--- scrapepdf.local.py
+++ scrapepdf.local.py
@@ -1,30 +1,41 @@
 import PyPDF2
+import undetected_chromedriver.v2 as uc
 import re
 import time
+import autoit
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
+chrome_options = uc.ChromeOptions()
 download_dir = r"C:\Users\grrbm\Desktop"
-chrome_options.add_experimental_option(
-    "prefs",
-    {
-        "download.default_directory": download_dir,
-        "download.prompt_for_download": False,
-        "download.directory_upgrade": True,
-        "plugins.always_open_pdf_externally": True,
-    },
-)
+# chrome_options.headless = True
+# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--blink-settings=imagesEnabled=false")
+chrome_options.add_argument("--print-to-pdf=" + download_dir)
 
-driver = webdriver.Chrome(
-    executable_path=r"C:\Python39\ChromeDriver\chromedriver", options=chrome_options
-)
-driver.get("https://www.google.com/")
-time.sleep(5)
+driver = uc.Chrome(options=chrome_options)
+# driver.get("https://www.google.com/")
+# time.sleep(5)
 file_name = "API-v-1.1.pdf"
 driver.get("https://foxbit.com.br/wp-content/uploads/2018/06/" + file_name)
 time.sleep(5)
+
+autoit.send("^p")
+
+time.sleep(5)
+
+autoit.send("{ENTER}")
+
+time.sleep(15)
+
+autoit.send("38294699000112")
+
+time.sleep(3)
+
+autoit.send("{ENTER}")
+
+time.sleep(10)
 
 file_name = "Boleto Simples Nacional.pdf"
 pathToRead = download_dir + "/" + file_name
@@ -38,4 +49,8 @@ result = re.search("Página:/(.*)AUTENTICAÇÃO MECÂNICA", p_text)
 
 num_boleto = result.group(1)
 
+pageSource = driver.page_source
+fileToWrite = open(download_dir + "/" + "page_source.html", "w")
+fileToWrite.write(pageSource)
+fileToWrite.close()
 print(num_boleto)
