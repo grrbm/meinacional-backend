48beee2986b79d71b2af169c02cb84a8a99a23d3
Guilherme Reis <grrbm2@gmail.com>
2021-09-03 16:09:06 -0300

    changed backend to accept processing payment code

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const { spawn } = require('child_process');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const { spawn } = require("child_process");
 
-app.get('/', (req, res) => {
-
+app.get("/", (req, res) => {
   var dataToSend;
   // spawn new child process to call the python script
-  const python = spawn('python', ['scraper.py']);
+  const python = spawn("python", ["scraper.py"]);
 
   // collect data from script
-  python.stdout.on('data', function (data) {
-    console.log('Pipe data from python script ...');
+  python.stdout.on("data", function (data) {
+    console.log("Pipe data from python script ...");
     dataToSend = data.toString();
   });
 
-  python.on('error', function(err) {
-    console.log('There was an error while pipeing data: ' + err);
-    res.send(err)
+  python.on("error", function (err) {
+    console.log("There was an error while pipeing data: " + err);
+    res.send(err);
   });
   // in close event we are sure that stream from child process is closed
-  python.on('close', (code, signal) => {
-    console.log(`child process close all stdio with code ${code} and signal ${signal}`);
+  python.on("close", (code, signal) => {
+    console.log(
+      `child process close all stdio with code ${code} and signal ${signal}`
+    );
     // send data to browser
-    res.send(dataToSend)
+    res.send(dataToSend);
   });
-})
+});
+
+app.get("/paymentCode", (req, res) => {
+  var dataToSend;
+  // spawn new child process to call the python script
+  const python = spawn("python", ["print.payment.py"]);
+
+  // collect data from script
+  python.stdout.on("data", function (data) {
+    console.log("Pipe data from python script ...");
+    dataToSend = data.toString();
+  });
+
+  python.on("error", function (err) {
+    console.log("There was an error while pipeing data: " + err);
+    res.send(err);
+  });
+  // in close event we are sure that stream from child process is closed
+  python.on("close", (code, signal) => {
+    console.log(
+      `child process close all stdio with code ${code} and signal ${signal}`
+    );
+    // send data to browser
+    res.send(dataToSend);
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
