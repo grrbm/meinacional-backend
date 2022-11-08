a7d2ecadfcfc93eab514724b5d2e78bb7b2171a4
Guilherme Reis <grrbm2@gmail.com>
2022-11-08 20:21:25 -0300

    finished the curl with json data

--- shell scripts/stress-test.sh
+++ shell scripts/stress-test.sh
@@ -4,6 +4,13 @@
 
 # This will run give curl command 200 times with max 10 jobs in parallel.
 
-seq 1 200 | xargs -I $ -n1 -P10 curl -X POST "http://localhost:4000/example"
 
-# printf '%s\n' {1..400} | xargs -I % -P 8 curl -I -X POST 'http://localhost:4000/example'
+#seq 1 200
+#seq 1 1 | xargs -I $ -n1 -P10 curl -X POST "http://localhost:4000/example"
+
+seq 1 1 | xargs -I $ -n1 -P10 curl \
+                            --header "Content-Type: application/json" \
+                            -X POST "http://143.198.132.139:5000/meiHistory" \
+                            --data '{"cnpj":"38294699000112"}'
+
+
