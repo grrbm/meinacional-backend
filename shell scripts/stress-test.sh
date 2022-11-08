c1eb75209cef0645d53af49973074496c731ed30
Guilherme Reis <grrbm2@gmail.com>
2022-11-08 20:23:09 -0300

    added correct params for stress testing

--- shell scripts/stress-test.sh
+++ shell scripts/stress-test.sh
@@ -3,12 +3,9 @@
 #Faster is to type sh and a space and then drag the file to the window and release the icon anywhere on the window.
 
 # This will run give curl command 200 times with max 10 jobs in parallel.
+#seq 1 200 | xargs -I $ -n1 -P10 curl -X POST "http://localhost:4000/example"
 
-
-#seq 1 200
-#seq 1 1 | xargs -I $ -n1 -P10 curl -X POST "http://localhost:4000/example"
-
-seq 1 1 | xargs -I $ -n1 -P10 curl \
+seq 1 20 | xargs -I $ -n1 -P3 curl \
                             --header "Content-Type: application/json" \
                             -X POST "http://143.198.132.139:5000/meiHistory" \
                             --data '{"cnpj":"38294699000112"}'
