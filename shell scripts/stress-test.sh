1609a59034e61a7847c3d1242fdb96729c0ff305
Guilherme Reis <grrbm2@gmail.com>
2022-11-12 20:32:58 -0300

    added break line to curl on stress test

--- shell scripts/stress-test.sh
+++ shell scripts/stress-test.sh
@@ -5,7 +5,7 @@
 # This will run give curl command 200 times with max 10 jobs in parallel.
 #seq 1 200 | xargs -I $ -n1 -P10 curl -X POST "http://localhost:4000/example"
 
-seq 1 20 | xargs -I $ -n1 -P3 curl \
+seq 1 20 | xargs -I $ -n1 -P3 curl -w "\n" \
                             --header "Content-Type: application/json" \
                             -X POST "http://143.198.132.139:5000/meiHistory" \
                             --data '{"cnpj":"38294699000112"}'
