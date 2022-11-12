de703dd4aceead4f66d1e5839de0682dad976abd
Guilherme Reis <grrbm2@gmail.com>
2022-11-12 19:35:25 -0300

    created stress-test-local

---
+++ shell scripts/stress-test-local.sh
@@ -0,0 +1,11 @@
+
+
+
+
+#To run: "sh stress-test-local.sh"
+
+
+seq 1 20 | xargs -I $ -n1 -P3 curl \
+                            --header "Content-Type: application/json" \
+                            -X POST "http://localhost:5000/meiHistory" \
+                            --data '{"cnpj":"38294699000112"}'
