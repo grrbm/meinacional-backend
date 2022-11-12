577e7320b126f3328da73152183a30a2090d03e6
Guilherme Reis <grrbm2@gmail.com>
2022-11-12 20:23:59 -0300

    fixed curl end line

--- shell scripts/stress-test-local.sh
+++ shell scripts/stress-test-local.sh
@@ -4,8 +4,7 @@
 
 #To run: "sh stress-test-local.sh"
 
-
-seq 1 20 | xargs -I $ -n1 -P3 curl \
+seq 1 20 | xargs -I $ -n1 -P3 curl -w "\n" \
                             --header "Content-Type: application/json" \
                             -X POST "http://localhost:5000/meiHistory" \
                             --data '{"cnpj":"38294699000112"}'
